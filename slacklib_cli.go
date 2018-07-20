1c5a6f5bdde82d1ecf8b5f0eb329fd5f408bf147
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-07-19 22:05:46 -0400

    fix default

--- slacklib_cli.go
+++ slacklib_cli.go
@@ -18,7 +18,7 @@ var hook_url string
 
 func main()  {
     flag.StringVar(&alert.Channel, "c", "#some_channel", "Pass a slack channel")
-    flag.StringVar(&alert.Text, "m", "#some_channel", "Pass a message")
+    flag.StringVar(&alert.Text, "m", "some message", "Pass a message")
     flag.StringVar(&hook_url, "h", "https://hooks.slack.com", "Pass a slack webhook url")
     flag.Parse()
 
